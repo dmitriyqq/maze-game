9d06b3bfd9b7d1a2d23d79c18b3fb696f4255e28
dmitriyqq <shulaykin1@gmail.com>
2021-01-06 04:38:15 +0300

    added grid drawing

---
+++ main.c
@@ -0,0 +1,107 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <stdio.h>
+
+
+void draw_map(int x, int y, int grid_x, int grid_y, int cell_size) {
+    for (int i = 0; i < grid_x+1; i++) {
+        int cx = x + i * cell_size;
+        al_draw_line(cx, y, cx, y + grid_y * cell_size, al_map_rgb_f(1, 0, 0), 1);    
+    }
+
+    for (int i = 0; i < grid_y+1; i++) {
+        int cy = y + i * cell_size;
+        al_draw_line(x, cy, x + grid_x * cell_size, cy, al_map_rgb_f(1, 0, 0), 1);    
+    }
+}
+
+int main()
+{
+    if(!al_init())
+    {
+        printf("couldn't initialize allegro\n");
+        return 1;
+    }
+
+    if(!al_install_keyboard())
+    {
+        printf("couldn't initialize keyboard\n");
+        return 1;
+    }
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    if(!timer)
+    {
+        printf("couldn't initialize timer\n");
+        return 1;
+    }
+
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    if(!queue)
+    {
+        printf("couldn't initialize queue\n");
+        return 1;
+    }
+
+    ALLEGRO_DISPLAY* disp = al_create_display(640, 480);
+    if(!disp)
+    {
+        printf("couldn't initialize display\n");
+        return 1;
+    }
+
+    ALLEGRO_FONT* font = al_create_builtin_font();
+    if(!font)
+    {
+        printf("couldn't initialize font\n");
+        return 1;
+    }
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool done = false;
+    bool redraw = true;
+    ALLEGRO_EVENT event;
+
+    al_start_timer(timer);
+    while(1)
+    {
+        al_wait_for_event(queue, &event);
+
+        switch(event.type)
+        {
+            case ALLEGRO_EVENT_TIMER:
+                // game logic goes here.
+                redraw = true;
+                break;
+
+            case ALLEGRO_EVENT_KEY_DOWN:
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                break;
+        }
+
+        if(done)
+            break;
+
+        if(redraw && al_is_event_queue_empty(queue))
+        {
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            draw_map(100, 100, 8, 8, 20);
+            al_flip_display();
+
+            redraw = false;
+        }
+    }
+
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    return 0;
+}
